861dddcb2b21815316e8714e68356852e55dd7e6
debasish <debasish.sahoo96@gmail.com>
2024-09-07 19:26:43 +0530

    counter

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Card from "./Components/Card";
-
+//import Card from "./Components/Card";
+import Counter from './Components/Counter'
 function App() {
   return (
     <>
-      <Card />
+      {/* <Card /> */}
+      <Counter/>
     </>
   );
 }
