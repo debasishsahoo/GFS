61869f24871e9ba816c4f8a853b187048d6da310
debasish <debasish.sahoo96@gmail.com>
2024-09-07 20:08:22 +0530

    last state is done

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 //import Card from "./Components/Card";
 //import Counter from './Components/Counter'
-import Clock from "./Components/Clock";
+//import Clock from "./Components/Clock";
+import ShowHide from './Components/ShowHide.jsx'
 function App() {
   return (
     <>
       {/* <Card /> */}
       {/* <Counter/> */}
-      <Clock />
+      {/* <Clock /> */}
+      <ShowHide/>
     </>
   );
 }
