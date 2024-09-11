097b646c8ddddff6b29d47cb0847f89939db87e5
debasish <debasish.sahoo96@gmail.com>
2024-09-11 19:41:55 +0530

    filter update

--- client/src/Components/list/FilterComponent.jsx
+++ client/src/Components/list/FilterComponent.jsx
@@ -26,6 +26,30 @@ const FilterComponent = () => {
 
   console.log(arr.filter(isPrime))
 
+
+const errVal=[
+    {id:15},{id:-1},{id:0},{id:3},{id:12.2},
+    {},{id:null},{id:NaN},{id:'undefined'},{id:22},
+    {id:17},{id:10},
+]
+let invalidEntries=0;
+function filterById(item){
+    if(Number.isFinite(item.id)&& item.id !==0 && item.id > 0){
+        return true;
+    }
+    invalidEntries++;
+    return false;
+}
+const arrById=errVal.filter(filterById);
+console.log('arrById:', arrById)
+console.log('invalidEntries:', invalidEntries)
+
+
+
+
+
+
+
   return <div>FilterComponent</div>;
 };
 
