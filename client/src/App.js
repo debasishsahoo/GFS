94f7193134346c7a3272dea8db60883d72a99985
debasish <debasish.sahoo96@gmail.com>
2024-09-21 20:55:48 +0530

    code is update

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 // import UseEffectHooks1 from './Components/ReactHOOKS/useEffect/UseEffectHooks1'
 // import UseEffectHooks2 from './Components/ReactHOOKS/useEffect/UseEffectHooks2'
 // import UseEffectHooks3 from './Components/ReactHOOKS/useEffect/UseEffectHooks3'
-import TableComponent from './Components/ReactHOOKS/useEffect/TableComponent'
+// import TableComponent from './Components/ReactHOOKS/useEffect/TableComponent'
+import TableComponent2 from './Components/ReactHOOKS/useEffect/TableComponent2'
 function App() {
   return (
     <>
     {/* <UseEffectHooks1/> */}
     {/* <UseEffectHooks2/> */}
     {/* <UseEffectHooks3/> */}
-    <TableComponent/>
+    {/* <TableComponent/> */}
+    <TableComponent2/>
     </>
   );
 }
--- client/src/Services/list.js
+++ client/src/Services/list.js
@@ -1,16 +1,15 @@
 //GET the Data from API(RECEIVED)
-export function getItemList(){
-    return fetch('http://localhost:3333/list')
-    .than(data=>data.json())
+export function getItemList() {
+  return fetch("http://localhost:3333/list").than((data) => data.json());
 }
 
 //SET the Data from API(SEND)
-export function setItem(item){
-   return fetch('http://localhost:3333/list',{
-    method:'POST',
-    headers:{
-        'Content-Type':'application/json'
+export function setItem(item) {
+  return fetch("http://localhost:3333/list", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
     },
-    body:JSON.stringify({item})
-   }).then(data=>data.json())
+    body: JSON.stringify({ item }),
+  }).then((data) => data.json());
 }
